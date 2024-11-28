b172beee028a30e9179a74f4aee384e5ed933087
Artem Chaika <artem7cha@gmail.com>
2024-11-28 21:15:46 +0200

    trial commit

---
+++ src/main/java/com/example/wishlist_site_admin/WishlistSiteAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlist_site_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistSiteAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistSiteAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wishlist_site_admin/WishlistSiteAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishlist_site_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistSiteAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
