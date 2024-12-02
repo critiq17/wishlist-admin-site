f6b0ffac01f96cdc2c35e8bbca0462be2b968498
critiq17 <critiq17@gmail.com>
2024-12-02 12:57:44 +0200

    small changes

---
+++ src/main/java/com/example/wishlist_site_admin/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.wishlist_site_admin.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
---
+++ src/main/java/com/example/wishlist_site_admin/controllers/MainController.java
@@ -0,0 +1,22 @@
+package com.example.wishlist_site_admin.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model, @AuthenticationPrincipal OidcUser principal) {
+        if (principal != null) {
+            model.addAttribute("profile", principal.getClaims());
+        }
+        return "index";
+    }
+}
